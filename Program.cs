0daa589cfe06bffc3d38d7d47d2f1d0945c13725
Ruhma-desai-786 <romakesari2734@gmail.com>
2023-05-27 17:21:42 +0530

    LogiaclProgramming

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Logical_programming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool isrun = true;
+            while (isrun)
+            {
+                Console.WriteLine("Select:\n1)Fibonacii Series \n2)Perfect Number \n3)Prime Number \n4)Reverse Number \n5)Coupon Number \n6)Stop Watch");
+                int option = Int32.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Fibonacci_Series_child obj = new Fibonacci_Series_child();
+                        obj.Series();
+                        obj.Display();
+                        break;
+                    case 2:
+                        Perfect_Number perfect_Number = new Perfect_Number();
+                        perfect_Number.PerfectNumber();
+                        break;
+                    case 3:
+                        Prime_Number Prime_Number = new Prime_Number();
+                        Prime_Number.PrimeNumber();
+                        break;
+                    case 4:
+                        Reverse_A_Number Reverse_A_Number = new Reverse_A_Number();
+                        Reverse_A_Number.ReverseNumber();
+                        break;
+                    case 5:
+                        Coupon_Number number = new Coupon_Number();
+                        number.Coupon();
+                        break;
+                    case 6:
+                        Stop_watch stop_Watch = new Stop_watch();
+                        stop_Watch.StopWatch();
+                        break;
+                    default:
+                        isrun = false;
+                        Console.WriteLine("select correct option");
+                        break;
+                }
+            }
+        }
+    }
+}
